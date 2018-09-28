4ee7a336b513d7fb927eb8b9ab3fafca0b4a00f9
KyMaKa <42968965+KyMaKa@users.noreply.github.com>
2018-09-28 21:38:27 +0700

    Eeeem...

---
+++ prime2000k.hs
@@ -0,0 +1,16 @@
+summ n = sumnd n
+        where sumnd n = nd' n
+              nd' 1 = 0
+              nd' 0 = 0
+              nd' y = if (prime y == True) then y + nd' (y - 1)
+                                                    else nd' (y - 1)
+              prime x = if prime' x == (x - 1) then True
+                            else False
+                            where
+                              prime' 0 = 0
+                              prime' y | mod x (y - 1) > 0 = 1 + prime' (y - 1)
+                                       | otherwise = 0
+                              mod x y | (y > x) = x
+                                      | y == 0 = 0
+                                      | otherwise = if y > 1 then mod (x-y) y
+                                        else 1
