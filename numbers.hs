f691d5b8a6a5ed92d0742c4c6cb52957e1f5aa69
KyMaKa <KyMaKa@github.com>
2018-10-22 20:28:26 +0700

    heh

--- numbers.hs
+++ numbers.hs
@@ -1,3 +1,4 @@
+import Data.Char
 toDecimal:: Int -> String -> String
 toDecimal base snumber | base < 10 = show (toDecimal' 1 snumber)
                        | base > 36 && base < 62 = show (toDecimal''' 1 snumber)
@@ -16,3 +17,25 @@ toDecimal base snumber | base < 10 = show (toDecimal' 1 snumber)
                               | fromEnum(head ys) >= 97 = (fromEnum (head ys) - 87)*(base^(length(snumber) - n)) + toDecimal''' (n+1) (tail ys)
                               | fromEnum(head ys) >= 65 = (fromEnum (head ys) - 29)*(base^(length(snumber) - n)) + toDecimal''' (n+1) (tail ys)
                               | otherwise = (fromEnum (head ys) - 48)*(base^(length(snumber) - n)) + toDecimal''' (n+1) (tail ys)
+
+fromDecimal:: Int -> String -> String
+fromDecimal toBase snumber | toBase < 10 = fromDecimal' (read snumber::Int) ""
+                           | toBase > 36 = fromDecimal''' (read snumber) ""
+                           | toBase > 10 && toBase < 37 = fromDecimal'' (read snumber::Int) ""
+                           | otherwise = snumber
+  where
+    fromDecimal' ys ss | ys >= toBase = fromDecimal' (div ys toBase) (show(mod ys toBase)++ss)
+                       | otherwise = show ys ++ ss
+
+    fromDecimal'' 0 ss = ss
+    fromDecimal'' ys ss | ys > toBase && ys >= 10 && ys `mod` toBase >= 10 = fromDecimal'' (div ys toBase) ((toEnum((mod ys toBase) + 87)::Char) : ss)
+                        | ys < 10 = (show(mod ys toBase)++ss)
+                        | ys == toBase || ys > toBase = fromDecimal'' (div ys toBase) (show(mod ys toBase)++ss)
+                        | otherwise = ((toEnum(ys + 87)::Char) : ss)
+
+    fromDecimal''' 0 ss = ss
+    fromDecimal''' ys ss | ys > toBase && ys >= 10 && ys `mod` toBase >= 10 = fromDecimal''' (div ys toBase) ((toEnum((mod ys toBase) + 87)::Char) : ss)
+                         | ys < 10 = (show(mod ys toBase)++ss)
+                         | ys == toBase || ys > toBase = fromDecimal''' (div ys toBase) (show(mod ys toBase)++ss)
+                         | ys > 35 = fromDecimal''' (div ys toBase) ((toEnum((mod ys toBase) + 29)::Char) : ss)
+                         | otherwise = ((toEnum(ys + 87)::Char) : ss)
